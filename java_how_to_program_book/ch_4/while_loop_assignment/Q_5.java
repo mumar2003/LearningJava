61b6a6233d4597f7cc5c970158e0f1ab304f13e1
mumar2003 <umarch4817@gmail.com>
2022-07-19 10:32:21 +0500

    i have found my mistake in q_5 and then completed it

--- java_how_to_program_book/ch_4/while_loop_assignment/Q_5.java
+++ java_how_to_program_book/ch_4/while_loop_assignment/Q_5.java
@@ -9,13 +9,42 @@ public class Q_5 {
         Scanner input = new Scanner(System.in);
         System.out.println("please enter the number");
         int N1 = input.nextInt();
-        int small = 0, large = 0;
+        int small=0, large=0, temp = N1;
+        int i=0;
+        int M1;
         while (N1 != 0) {
-
-            if (N1 == 0) {
+            System.out.println("this program will be taking input util you enter 0");
+            System.out.println("Please Enter the Number");
+            M1 = input.nextInt();
+            if (M1 == 0) {
+                System.out.println("You Enter 0 so your program finished");
+                N1 = M1;
                 break;
+            } else {
+                 while(i<2) {
+                     if (M1 > N1) {
+                         large=M1;
+                     }else{
+                             large=N1;
+                         }
+                     if(M1<N1){
+                       small=M1;
+                     }else {
+                         small=N1;
+                     }
+                     i++;
+                     }
+                 if(M1>large){
+                     large=M1;
+                 }else{
+                     if(M1<small){
+                         small=M1;
+                     }
+                 }
+                 }
+                 }
+        System.out.println("small number is = "+small);
+        System.out.println("large number is = "+large);
             }
 
-        }
     }
-}
