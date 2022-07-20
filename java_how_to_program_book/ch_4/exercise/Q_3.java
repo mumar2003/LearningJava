ab285e2ad847d19a989aab20821812e775d8ce74
mumar2003 <umarch4817@gmail.com>
2022-07-20 13:37:41 +0500

    add Q #3 ub chapter 4

---
+++ java_how_to_program_book/ch_4/exercise/Q_3.java
@@ -0,0 +1,22 @@
+//4.3 Write four different Java statements that each add 1 to integer variable x.
+ package java_how_to_program_book.ch_4.exercise;
+
+public class Q_3 {
+    public static void main(String[] args) {
+        int x=0;
+        x=x+1;
+        System.out.println(x +"\n");
+        x=0;
+        x+=1;
+        System.out.println(x+ "\n");
+        x=0;
+        x++;
+        System.out.println(x+ "\n");
+        x=0;
+        ++x;
+        System.out.println(x+ "\n");
+
+
+
+    }
+}
