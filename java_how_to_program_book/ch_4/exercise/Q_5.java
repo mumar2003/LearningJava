90b0767c63a10a8f201db6e7168146d4de2d46ca
mumar2003 <umarch4817@gmail.com>
2022-07-20 15:05:35 +0500

    solve chapter 4 Q_5

---
+++ java_how_to_program_book/ch_4/exercise/Q_5.java
@@ -0,0 +1,19 @@
+//4.5 Write a Java statement to accomplish each of the following tasks:
+// 1. Declare variable sum of type int and initialize it to 0.
+//2. Declare variable x of type int and initialize it to 1.
+//3. Add variable x to variable sum, and assign the result to variable sum.
+//4. Print "The sum is: ", followed by the value of variable sum.
+package java_how_to_program_book.ch_4.exercise;
+
+public class Q_5 {
+    public static void main(String[] args) {
+        // 1. Declare variable sum of type int and initialize it to 0.
+        int sum=0;
+        //2. Declare variable x of type int and initialize it to 1.
+        int x=1;
+        // 3.Add variable x to variable sum, and assign the result to variable sum.
+        sum=sum+x;
+        //4. Print "The sum is: ", followed by the value of variable sum.
+        System.out.println("The sum is: "+sum);
+    }
+}
