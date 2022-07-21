c114c0cb75dec9c71ed2c49362dc2473f9a7a76d
mumar2003 <umarch4817@gmail.com>
2022-07-21 11:04:27 +0500

    solve chapter 4 Q_22

--- java_how_to_program_book/ch_4/exercise/Q_22.java
+++ java_how_to_program_book/ch_4/exercise/Q_22.java
@@ -1,4 +1,20 @@
+/*
+4.22 (Tabular Output) Write a Java application that uses looping to print
+the following table of values:
+
+ */
 package java_how_to_program_book.ch_4.exercise;
 
+
 public class Q_22 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 5) {
+            if (i == 1) {
+                System.out.println("    N    N*10   N*100   N*1000");
+            }
+            System.out.println("    " + i + "    " + i * 10 + "    " + i * 100 + "      " + i * 1000);
+            i++;
+        }
+    }
 }
