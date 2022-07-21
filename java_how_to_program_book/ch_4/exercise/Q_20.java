4642ea356725d1c7f7bdc5324c2eea29dfae5cfc
mumar2003 <umarch4817@gmail.com>
2022-07-21 10:27:39 +0500

    solve chapter 4 Q_20

--- java_how_to_program_book/ch_4/exercise/Q_20.java
+++ java_how_to_program_book/ch_4/exercise/Q_20.java
@@ -11,5 +11,30 @@ display the employee’s gross pay. Use class Scanner to input the data.
 
 package java_how_to_program_book.ch_4.exercise;
 
+import java.util.Scanner;
+
 public class Q_20 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        float i, hours, rates, salary;
+        int j = 1;
+        while (j != 0) {
+            System.out.println("enter your working hours in a week");
+            hours = input.nextFloat();
+            if (hours > 40) {
+                i = (float) ((hours - 40) * 1.5);
+                hours = 40 + i;
+                System.out.println(hours);
+            }
+            System.out.println("Enter your working hourly rates ");
+            rates = input.nextInt();
+            salary = rates * hours;
+            System.out.println("Your salary is = " + salary);
+            System.out.println("for finish program enter 0 for continue enter 1");
+            j = input.nextInt();
+        }
+
+    }
+
 }
