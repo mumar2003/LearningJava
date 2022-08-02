888bef57eaa7c5f130fe622c5e2b1644c36bfe08
mumar2003 <umarch4817@gmail.com>
2022-08-02 21:45:42 +0500

    chapter 6 Q#6.11

---
+++ java_how_to_program_book/ch_6/Q_11.java
@@ -0,0 +1,19 @@
+/*
+6.11 Answer each of the following questions:
+1. What does it mean to choose numbers “at random”?
+2. Why is the nextInt method of class SecureRandom useful
+for simulating games of chance?
+3. Why is it often necessary to scale or shift the values produced bya SecureRandom object?
+ */
+package java_how_to_program_book.ch_6;
+import java.util.Scanner;
+public class Q_11 {
+    public static Scanner input=new Scanner(System.in);
+    public static void main(String[] args) {
+    /*
+       1. randomNumber mean equal chance of everyNumber that we want
+       2. because it Produces randomNumber
+       3. for range managing
+     */
+    }
+}
